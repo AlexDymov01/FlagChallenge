ed3946a047596cefabd7c7111bdeb5e6399c3e84
Dymov Oleksandr <dymovoleksandr.2002@gmail.com>
2025-02-11 14:41:03 +0200

    Initial Commit

---
+++ Flag Challenge/Flag_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flag_ChallengeApp.swift
+//  Flag Challenge
+//
+//  Created by Sasha Dymov  on 11.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Flag_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
